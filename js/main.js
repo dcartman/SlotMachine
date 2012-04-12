d5255d26a1251015a5582ed6e6e04861a4d5d874
Daniel Carter <daniel.carter@ddotb.com>
2012-04-11 21:21:16 -0700

    Updated the slot machine engine

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
 $(document).ready(function() {
 	var sl = new SlotMachine( document.getElementById('slot-machine'), {} )
 });
+
+dpc = {
+	isConsoleOn: false,
+	log: function( msg ) {
+		try {
+			if(this.isConsoleOn) {
+				var log = this.createNode({ "type":"div","content":msg });
+				this.console = this.console || document.getElementById("console");
+				if( !this.console ) { 
+					this.console = this.createNode({ "type": "div", "attributes": [{ "name":"id", "value":"console" }] });
+					document.body.appendChild( this.console );
+				}
+				this.console.appendChild( log );
+			}
+		} catch( error ) {
+			console.log(error);
+		}
+	},
+	clearLog: function() {
+		this.console.innerHTML = "";
+		document.body.removeChild( this.console );
+	},
+	createNode: function( node ) {
+		var newNode = document.createElement( node.type );
+		if( node.attributes && node.attributes.forEach ) {
+			node.attributes.forEach( function(attr) {
+				newNode.setAttribute(attr.name, attr.value);
+			}, this);
+		}
+		if( node.children && node.children.forEach ) {
+			node.children.forEach( function(child) {
+				newNode.appendChild( this.createNode(child) );
+			}, this);
+		}
+		if( node.content ) {
+			newNode.innerHTML += node.content;
+		}
+		return newNode;
+	},
+	turnConsoleOn: function() { this.isConsoleOn = true },
+	turnConsoleOff: function() { this.console = null; this.isConsoleOff = false; }
+}
--- js/slotmachine.js
+++ js/slotmachine.js
@@ -69,6 +69,7 @@ function SlotMachine(ele, $h) {
     this.wheels = [];
 
     this.setWheels();
+	this.setWheelHousingHeight();
     this.setTrigger();
 }
 
@@ -81,10 +82,20 @@ SlotMachine.prototype.setWheels = function() {
 
 SlotMachine.prototype.setTrigger = function() {
     var me = this;
-    $(me.element).find('.trigger').each(function(i, trigger) {
+    $(me.element).find('.trigger .spin').each(function(i, trigger) {
         me.trigger = trigger;
         trigger.object = me;
     }).bind('mouseup', me.initiateSpinCycle);
+    
+    $(me.element).find('.trigger .slide_up').each(function(i, trigger) {
+        me.slide_up = trigger;
+        trigger.object = me;
+    }).bind('mouseup', me.slideUp);
+    
+    $(me.element).find('.trigger .slide_down').each(function(i, trigger) {
+        me.slide_up = trigger;
+        trigger.object = me;
+    }).bind('mouseup', me.slideDown);
 }
 
 SlotMachine.prototype.initiateSpinCycle = function() {
@@ -95,6 +106,33 @@ SlotMachine.prototype.initiateSpinCycle = function() {
     });
 
 };
+
+SlotMachine.prototype.slideUp = function() {
+    var me = this.object;
+
+    me.wheels.forEach(function(wheel) {
+    	var allContent = wheel.element.querySelectorAll('.content');
+        wheel.slideUp(allContent);
+    });
+
+};
+
+SlotMachine.prototype.slideDown = function() {
+    var me = this.object;
+
+    me.wheels.forEach(function(wheel) {
+    	var allContent = wheel.element.querySelectorAll('.content');
+        wheel.slideDown(allContent);
+    });
+
+};
+
+
+SlotMachine.prototype.setWheelHousingHeight = function() {
+	var newHeight = this.element.querySelector('.wheel').offsetWidth * 2;
+	$(this.element).find('.wheel-housing').css( 'height', newHeight+"px" );
+	$(this.element).find('.wheel').css('height', "100%");
+}
 
 /* complimentary object to SlotMachine */
 function Wheel(ele, $h) {
@@ -107,6 +145,8 @@ function Wheel(ele, $h) {
     }
     this.id = Math.random();
     this.element.object = this;
+    this.spinning = false;
+    
     me.contentItems = [];
 
     me.getContent();
@@ -115,31 +155,27 @@ function Wheel(ele, $h) {
 /* This will handle spinning the wheels when the trigger has been pressed */
 Wheel.prototype.spin = function() {
     var me = this;
-    this.firstContent = this.element.querySelector('.content');
-    this.seconds = this.generateRandomTime(10);
-    this.spinSpeed = this.generateRandomTime(100);
-    this.startTime = Date.now();
+	if(this.spinning == false) {
+	    this.firstContent = this.element.querySelector('.content');
+	    this.seconds = 15; //this.generateRandomTime(20, 15);
+	    this.spinSpeed = this.generateRandomTime( this.element.offsetHeight / 2, this.element.offsetHeight / 4 );
+	    this.startTime = Date.now();
 
-    this.spinTimer = window.setInterval(
-    function(me) {
-        $(me.firstContent).trigger('wheel:spin');
-    }
-    ,
-    10, me);
-
-    window.setTimeout(
-    function() {
-        window.clearInterval(me.spinTimer);
-        me.chooseFinalSlide();
-    },
-    this.seconds * 1000);
+    	this.spinTimer = window.setInterval(
+    	function(me) {
+    	    $(me.firstContent).trigger('wheel:spin');
+    	}
+    	,
+    	10, me);
+    	this.spinning = true;
+	}
 }
 
 /* Handles moving the content items up by returning the the appropriate point in a sin curve */
 Wheel.prototype.rotateContentStep = function(t, b, c, d) {
     t = t || (Date.now() - this.startTime) / 1000;
     d = d || this.seconds;
-    speed = Math.round((Math.pow(Math.sin(t / (d * (1 / 3))), 4)) * this.spinSpeed) + 10;
+    speed = Math.round( ( Math.pow( ( Math.sin( Math.pow( ( ( t - (d*2.5) ) / (d*1.5) ), 3 ) - 2 ) + 1 ), 3) ) * this.spinSpeed );
     return speed;
 
 };
@@ -148,24 +184,47 @@ Wheel.prototype.rotateContentStep = function(t, b, c, d) {
 Wheel.prototype.chooseFinalSlide = function() {
     var me = this,
     allContent = me.element.querySelectorAll('.content');
-    if (allContent[0].offsetTop < (allContent[0].offsetHeight / 2)) {
+    if (Math.abs( allContent[0].offsetTop ) < (allContent[0].offsetHeight / 2)) {
         me.slideDown(allContent);
     } else {
-        me.slideUp(allContent);
+        me.slideUp(allContent); // <----- should be slideUp but its broken.
     }
 }
 
 /* Slide content up to the next full piece of content */
 Wheel.prototype.slideUp = function(allContent) {
-    allContent[1].style.marginTop = "0px";
-    me.element.removeChild(allContent[0]);
-    allContent[0].style.marginTop = "0px";
-    me.element.appendChild(allContent[0]);
+	var me = this,
+		time = ( Math.abs(allContent[1].offsetTop) / 100 );
+	this.setTransition( allContent[0], time, function(eve) {
+		var evEle = eve.currentTarget;
+		me.element.removeChild(evEle);
+    	evEle.style.marginTop = "0px";
+    	me.element.appendChild(evEle);
+	});
+    allContent[0].style.marginTop = "-"+ me.element.offsetHeight +"px";
 };
 
 /* Slide content down to the next full piece of content */
 Wheel.prototype.slideDown = function(allContent) {
-    allContent[0].style.marginTop = "0px";
+	var time = ( Math.abs(allContent[0].offsetTop) / 100 ),
+		el = allContent[0];
+	if(this.spinning == false) {
+		el = allContent[ allContent.length - 1 ];
+		this.element.removeChild(el);
+		el.style.marginTop = "-"+ this.element.offsetHeight +"px";
+	}
+	this.setTransition( el, time );
+    el.style.marginTop = "0px";
+};
+
+Wheel.prototype.setTransition = function( ele, time, callback) {
+	var me = this;
+	ele.style.WebkitTransition = "margin-top "+ time +"s ease-out";
+	ele.addEventListener('webkitTransitionEnd', function(event) {
+		event.currentTarget.style.webkitTransition = "";
+		me.spinning = false;
+		if( typeof(callback) == "function" ) callback.call(this, event);
+	});
 };
 
 /* Moves the top image to the bottom of the stack */
@@ -181,9 +240,12 @@ Wheel.prototype.moveToBottom = function(ele) {
     this.element.appendChild(ele);
 }
 
+
 /* used by Wheel.spin to generate a randome number of seconds */
-Wheel.prototype.generateRandomTime = function(x) {
-    return Math.round( Math.random() * x + 5 )
+Wheel.prototype.generateRandomTime = function(x, y) {
+	var x = ( typeof x == "undefined" || x < 10 ) ? 10 : x,
+		y = y || 5;
+    return Math.round( Math.random() * ( x -  y ) + 5)
 };
 
 /* Adds a custom event listener wheel:spin to each piece of content */
@@ -191,14 +253,20 @@ Wheel.prototype.setupContent = function(content) {
     var me = this;
     return $(content).bind('wheel:spin',
     function(event) {
-        var ele = event.currentTarget;
-
-        if (ele.offsetTop < 0 && ele.offsetHeight <= Math.abs(ele.offsetTop)) {
+        var ele = event.currentTarget,
+            rotateStep = me.rotateContentStep();
+	if( rotateStep < 1 ) {
+	
+	    window.clearInterval(me.spinTimer);
+            me.chooseFinalSlide();
+	 
+        } else if (ele.offsetTop < 0 && ele.offsetHeight <= Math.abs(ele.offsetTop)) {
 
             me.moveToBottom(ele);
 
         } else {
-            ele.style.marginTop = (ele.offsetTop - me.rotateContentStep()) + "px";
+ 
+            ele.style.marginTop = (ele.offsetTop - ( ( rotateStep <= (ele.offsetHeight / 2) ) ? rotateStep : (ele.offsetHeight / 2) ) ) + "px";
         }
     });
 }
